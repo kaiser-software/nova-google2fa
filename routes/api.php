3eb595117bc5871e0fcfbc8be03b0b260a9ac6f3
Leonard Smith <lenrsmith@gmail.com>
2020-01-02 13:35:01 -0600

    Add nova to paths and routes

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 /**
  * This route is called when user must first time confirm secret
  */
-Route::post('register', 'Lifeonscreen\Google2fa\Google2fa@register');
+Route::post('nova/register', 'Lifeonscreen\Google2fa\Google2fa@register');
 
 /**
  * This route is called when user must first time confirm secret
  */
-Route::post('confirm', 'Lifeonscreen\Google2fa\Google2fa@confirm');
+Route::post('nova/confirm', 'Lifeonscreen\Google2fa\Google2fa@confirm');
 
 /**
  * This route is called to verify users secret
  */
-Route::post('authenticate', 'Lifeonscreen\Google2fa\Google2fa@authenticate');
+Route::post('nova/authenticate', 'Lifeonscreen\Google2fa\Google2fa@authenticate');
